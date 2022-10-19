0668dd79af128f9f9a1ebc544cc44c07d468d75f
Ilya <efimov4242@gmail.com>
2022-10-19 12:48:34 +0300

    Добавлена задача №2 - Извлечение первого и последнего элемента строки

--- Task19102022/Program.cs
+++ Task19102022/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var one = "Naharis";
+var two = "Mormont";
+var three = "Sand";
+// BEGIN (write your solution here)
+Console.WriteLine($"{one[2]} {two[1]} {three[3]} {two[4]} {two[2]}");
+// END
+
+
+
+// Задача №2
+// Выведите на экран первую и последнюю буквы предложения, записанного в переменную text.
+var text = "Never forget what you are, for surely the world will not";
+// BEGIN (write your solution here)
+var lastIndex = text.Length - 1;
+Console.WriteLine($"First: {text[0]}");
+Console.WriteLine($"Last: {text[lastIndex]}");
+// END
